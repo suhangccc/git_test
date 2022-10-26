23cfe8e350f44006b7a65d67084a4413a394b3b1
suhang <2671090579@qq.com>
2022-10-26 10:37:45 +0800

    hot-fix commit1

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -3,7 +3,8 @@ package com.example.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello gi2");
+        System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
