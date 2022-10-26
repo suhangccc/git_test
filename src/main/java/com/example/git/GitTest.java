29e6e06eb9215a59cae7d8f10b77f1e7cc8fc85f
suhang <2671090579@qq.com>
2022-10-26 10:41:27 +0800

    master commit2

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -2,6 +2,7 @@ package com.example.git;
 
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("master test");
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
